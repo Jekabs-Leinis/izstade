63944a0d4d90155bbb64a85dfde848adcb510b1d
egilsvernavskk <73436832+egilsvernavskk@users.noreply.github.com>
2021-12-10 18:37:33 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={styles.card styles.list}
           >
             <h2>Deploy &rarr;</h2>
             <p>
