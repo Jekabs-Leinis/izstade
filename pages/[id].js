3eae13c9e42b0902d0bddbd0ce752283d2ba05cf
egils.vernavs <egils.vernavs.kk@gmail.com>
2022-03-13 12:45:20 +0200

    Make it better

--- pages/[id].js
+++ pages/[id].js
@@ -1,25 +1,24 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from '../styles/SameLevel.module.css'
 import {useRouter} from "next/router";
 import mapping from '../config/mapping.json'
 
 export default function Voice() {
     const router = useRouter();
-    const { id } = router.query;
+    const {id} = router.query;
+    const [hasError, setHasError] = useState(false);
 
     useEffect(() => {
-        if (Object.keys(mapping).includes(id)) {
-            router.push(mapping[id]);
+        if (id !== undefined) {
+            if (Object.keys(mapping).includes(id)) {
+                router.push(mapping[id]);
+            } else {
+                setHasError(true);
+            }
         }
     }, [id]);
 
-    return(
-        <div className={styles.container}>
-            {
-                Object.keys(mapping).includes(id)
-                    ? (<div></div>)
-                    : (<p className={styles.p}>The site that you are looking for doesn&apos;t exist</p>)
-            }
-        </div>
+    return (hasError
+            ? (<p className={styles.p}>The site that you are looking for doesn&apos;t exist</p>) : <div></div>
     );
 };
