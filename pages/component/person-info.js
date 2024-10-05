92825ff0e2639904e087ca57b5e9b6befad6f351
Jekabs Leinis <jekabs@leinis.com>
2024-10-05 17:10:37 +0300

    Add jp language

--- pages/component/person-info.js
+++ pages/component/person-info.js
@@ -5,37 +5,42 @@ import ReactHtmlParser from 'react-html-parser';
 
 import lv_data from '../../config/lv.json'
 import en_data from '../../config/en.json'
+import jp_data from '../../config/jp.json'
 
 export default function PersonInfo() {
   const router = useRouter();
-  const { id } = router.query;
+  const {id} = router.query;
   const name = lv_data[id]?.name;
   const description_lv = lv_data[id]?.description;
   const description_en = en_data[id]?.description;
+  const description_jp = jp_data[id]?.description;
 
   function toHtml(value) {
     return ReactHtmlParser(value);
   }
 
   function renderPersonCard() {
-      return (
-        <div>
-          <h1 className={styles.h1}>{toHtml(name)}</h1>
-          <p className={styles.p}>{toHtml(description_lv)}</p>
-          <br/>
-          <br/>
-          <p className={styles.p}>{toHtml(description_en)}</p>
-        </div>
-      )
+    return (
+      <div>
+        <h1 className={styles.h1}>{toHtml(name)}</h1>
+        <p className={styles.p}>{toHtml(description_jp)}</p>
+        <br/>
+        <br/>
+        <p className={styles.p}>{toHtml(description_en)}</p>
+        <br/>
+        <br/>
+        <p className={styles.p}>{toHtml(description_lv)}</p>
+      </div>
+    )
   }
 
-  if(id) {
-      return (<div className={styles.container}>
-          <main className={[styles.main, styles.newLine].join(" ")}>
-              {renderPersonCard()}
-          </main>
-      </div>)
+  if (id) {
+    return (<div className={styles.container}>
+      <main className={[styles.main, styles.newLine].join(" ")}>
+        {renderPersonCard()}
+      </main>
+    </div>)
   } else {
-      return (<div></div>)
+    return (<div></div>)
   }
 };
--- pages/info/japan.js
+++ pages/info/japan.js
@@ -1,13 +1,16 @@
 ﻿import styles from '../../styles/Home.module.css'
 import lv_data from '../../config/lv.json'
 import en_data from '../../config/en.json'
+import jp_data from '../../config/jp.json'
 import ReactHtmlParser from 'react-html-parser'
 
 export default function Home() {
     const description_lv = lv_data['info_japan']?.description;
     const description_en = en_data['info_japan']?.description;
+    const description_jp = jp_data['info_japan']?.description;
     const name_lv = lv_data['info_japan']?.name;
     const name_en = en_data['info_japan']?.name;
+    const name_jp = jp_data['info_japan']?.name;
 
     function toHtml(value) {
         return ReactHtmlParser(value);
@@ -16,15 +19,27 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <main className={[styles.main, styles.newLine].join(" ")}>
-                <div className={styles.alignLeft}><i>Scroll down for english</i></div><br/>
-                <div className={[styles.alignLeft, styles.h2].join(" ")}>LV</div><br/>
-                <div className={styles.h1}>{toHtml(name_lv)}</div><br/>
-                <div>{toHtml(description_lv)}</div>
+                <div className={styles.alignLeft}><i>Scroll down for english end latvian</i></div>
+                <br/>
+                <div className={[styles.alignLeft, styles.h2].join(" ")}>JP</div>
+                <br/>
+                <div className={styles.h1}>{toHtml(name_jp)}</div>
+                <br/>
+                <div>{toHtml(description_jp)}</div>
                 <br/>
                 <br/>
-                <div className={[styles.alignLeft, styles.h2].join(" ")}>ENG</div><br/>
-                <div className={styles.h1}>{toHtml(name_en)}</div><br/>
+                <div className={[styles.alignLeft, styles.h2].join(" ")}>ENG</div>
+                <br/>
+                <div className={styles.h1}>{toHtml(name_en)}</div>
+                <br/>
                 <div>{toHtml(description_en)}</div>
+                <br/>
+                <br/>
+                <div className={[styles.alignLeft, styles.h2].join(" ")}>LV</div>
+                <br/>
+                <div className={styles.h1}>{toHtml(name_lv)}</div>
+                <br/>
+                <div>{toHtml(description_lv)}</div>
             </main>
         </div>
     )
