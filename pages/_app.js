12cec23ecddeb73a52de2090e24ce117dd607552
Jekabs Leinis <jekabs@leinis.com>
2024-12-19 23:14:04 +0200

    Fix google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,18 @@
 import '../styles/globals.css'
 import {Analytics} from "@vercel/analytics/react";
 import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 function MyApp({Component, pageProps}) {
   return (
     <div>
       <Head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YRYEFWNE43"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-YRYEFWNE43');
-        </script>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-YRYEFWNE43" strategy="beforeInteractive"/>
       </Head>
       <Component {...pageProps} />
       <Analytics/>
+      <GoogleAnalytics gaId="G-YRYEFWNE43"/>
     </div>
   )
 }
