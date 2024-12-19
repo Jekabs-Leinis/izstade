301fad2018322d9cef11f0ee3359db8c73a1ca59
Jekabs Leinis <jekabs@leinis.com>
2024-12-19 23:14:21 +0200

    Clenaup

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
 import '../styles/globals.css'
 import {Analytics} from "@vercel/analytics/react";
-import Head from "next/head";
 import { GoogleAnalytics } from '@next/third-parties/google'
-import Script from "next/script";
 
 function MyApp({Component, pageProps}) {
   return (
     <div>
-      <Head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-YRYEFWNE43" strategy="beforeInteractive"/>
-      </Head>
       <Component {...pageProps} />
       <Analytics/>
       <GoogleAnalytics gaId="G-YRYEFWNE43"/>
