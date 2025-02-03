b85c59970117222e763ba1497bdf412ed15f07cd
Jēkabs Leinis <jekabsleinis@gmail.com>
2025-02-03 13:44:34 +0200

    Update audio-player.js

--- pages/component/audio-player.js
+++ pages/component/audio-player.js
@@ -35,7 +35,7 @@ export default function AudioPlayer() {
   }, [id, isPlaying]);
 
   function startMcorpApp() {
-    let app = MCorp.app("509180010425611002", {anon: true});
+    let app = MCorp.app("3122177865748783959", {anon: true});
     app.run = function () {
       let motion = app.motions["iru-master"];
       
